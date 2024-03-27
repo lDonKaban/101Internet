ace3ef4e6763fcd392d24f64fe1499c5088929e7
Sasha <lDonKabanl@yandex.ru>
2024-03-28 01:06:13 +0300

    wip (testTask): добавлено обозначение цветом самых выгодных полей, добавлен столбец с преимуществами тарифа

--- src/Components/Page.js
+++ src/Components/Page.js
@@ -31,9 +31,35 @@ const useStyles = makeStyles((theme) => ({
   },
   menuPaper: {
     maxHeight: 500,
-  },
+  }
 }));
 
+const getColor = (arr) => arr.length > 1 ? 'blue' : 'green';
+// const getBestCondition = (param) => {
+//   let result
+//   if (param === 'displayPrice') {
+//     result = tariffsData.reduce((acc, tariff) => {
+//       const value = tariff[param] || Infinity
+//       if (acc.value < value) return acc
+//       if (acc.value === value) {
+//         acc.id.push(tariff.id)
+//         return acc
+//       }
+//       return acc = {id: [tariff.id], value}
+//     }, {id: [], value: Infinity})
+//   } else {
+//     result = tariffsData.reduce((acc, tariff) => {
+//       const speed = param === 'speed_in' ? tariff.internet?.speed_in || 0
+//       if (acc?.speed > speed) return acc
+//       if (acc?.speed === speed) {
+//         acc.id.push(tariff.id)
+//         return acc
+//       }
+//       return acc = {id: [tariff.id], speed}
+//     }, {id: [], speed: -1});
+//   }
+//   return result
+// }
 function Page() {
   const classes = useStyles();
 
@@ -60,10 +86,52 @@ function Page() {
   });
   const tariffsData = tariffs?.data?.tariffs?.data || [];
   const bestInternetSpeed = tariffsData.reduce((acc, tariff) => {
-    return acc.speed > tariff.internet?.speed_in ? acc : {id: tariff.id, speed: tariff.internet?.speed_in}
-  }, {speed: -1})
-  console.log('test =', bestInternetSpeed)
+    const speed = tariff.internet?.speed_in || 0
+    if (acc?.speed > speed) return acc
+    if (acc?.speed === speed) {
+      acc.id.push(tariff.id)
+      return acc
+    }
+    return acc = {id: [tariff.id], speed}
+  }, {id: [], speed: -1});
 
+  bestInternetSpeed.color = getColor(bestInternetSpeed.id);
+
+  const bestPrice = tariffsData.reduce((acc, tariff) => {
+    const price = tariff.displayPrice || Infinity
+    if (acc.price < price) return acc
+    if (acc.price === price) {
+      acc.id.push(tariff.id)
+      return acc
+    }
+    return acc = {id: [tariff.id], price}
+  }, {id: [], price: Infinity})
+
+  bestPrice.color = getColor(bestPrice.id)
+
+  const bestCountTV = tariffsData.reduce((acc, tariff) => {
+    const countTV = tariff.tv?.channels || 0
+    if (acc.count > countTV) return acc
+    if (acc.count === countTV) {
+      acc.id.push(tariff.id)
+      return acc
+    }
+    return acc = {id: [tariff.id], count: countTV}
+  }, {id: [], count: -1})
+
+  bestCountTV.color = getColor(bestCountTV.id)
+
+  const bestCountHDTV = tariffsData.reduce((acc, tariff) => {
+    const countTV = tariff.tv?.channels_hd || 0
+    if (acc.count > countTV) return acc
+    if (acc.count === countTV) {
+      acc.id.push(tariff.id)
+      return acc
+    }
+    return acc = {id: [tariff.id], count: countTV}
+  }, {id: [], count: -1})
+
+  bestCountHDTV.color = getColor(bestCountHDTV.id)
   const handleChange = (event) => {
     const foundProvider = providersData.find(
       (x) => x.id === +event.target.value
@@ -72,6 +140,15 @@ function Page() {
       setCurrentProvider(foundProvider);
     }
   };
+
+  const getAdvantage = (id) => {
+    const advantages = []
+    if (bestInternetSpeed.id.includes(id)) advantages.push('высокая скорость интернета')
+    if (bestPrice.id.includes(id)) advantages.push('низкая цена')
+    if (bestCountHDTV.id.includes(id)) advantages.push('большое количество HD-каналов')
+    if (bestCountTV.id.includes(id)) advantages.push('большое количество телеканалов')
+    return advantages.join(', ')
+  }
 
   return (
     <Container>
@@ -109,6 +186,7 @@ function Page() {
               <TableCell>Количество телеканалов</TableCell>
               <TableCell>Количество HD-каналов</TableCell>
               <TableCell>Цена</TableCell>
+              <TableCell>Преимущества</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
@@ -117,17 +195,39 @@ function Page() {
                 <TableCell component="th" scope="row">
                   {tariff.name || '-'}
                 </TableCell>
-                <TableCell component="th" scope="row">
+                <TableCell
+                    component="th"
+                    scope="row"
+                    style={{backgroundColor: bestInternetSpeed.id?.includes(tariff.id) ? bestInternetSpeed.color : ''}}
+                >
                   {tariff.internet?.speed_in || '-'}
                 </TableCell>
-                <TableCell component="th" scope="row">
+                <TableCell
+                    component="th"
+                    scope="row"
+                    style={{backgroundColor: bestCountTV.id?.includes(tariff.id) ? bestCountTV.color : ''}}
+                >
                   {tariff.tv?.channels || '-'}
                 </TableCell>
-                <TableCell component="th" scope="row">
+                <TableCell
+                    component="th"
+                    scope="row"
+                    style={{backgroundColor: bestCountHDTV.id?.includes(tariff.id) ? bestCountHDTV.color : ''}}
+                >
                   {tariff.tv?.channels_hd || '-'}
                 </TableCell>
-                <TableCell component="th" scope="row">
+                <TableCell
+                    component="th"
+                    scope="row"
+                    style={{backgroundColor: bestPrice.id?.includes(tariff.id) ? bestPrice.color : ''}}
+                >
                   {tariff?.displayPrice || '-'}
+                </TableCell>
+                <TableCell
+                    component="th"
+                    scope="row"
+                >
+                  {getAdvantage(tariff.id)}
                 </TableCell>
               </TableRow>
             ))}
